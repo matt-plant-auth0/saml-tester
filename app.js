ffcc24c56f5648a6bf0b92a8c40687e86b3df85c
matt-plant-auth0 <matthew.plant@auth0.com>
2021-10-08 16:56:26 +0100

    added management API call to get SAML config

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var samlRouter = require('./routes/saml');
 var jwtRouter = require('./routes/jwt');
 var userDetailsRouter = require('./routes/userdetails');
+var apiRouter = require('./routes/api');
 
 var {
   checkUrl,
@@ -39,6 +40,7 @@ app.use('/', indexRouter);
 app.use('/saml', samlRouter);
 app.use('/jwt', jwtRouter);
 app.use('/userdetails', userDetailsRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,17 @@ var crypto = require('crypto');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Auth0 SAML Demo' });
+router.get('/', async function(req, res, next) {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const res = await fetch('/api/saml-config', options);
+  const json = await res.json();
+  res.render('index', { title: 'Auth0 SAML Demo', samlSettings: json });
 });
 
 module.exports = router;
