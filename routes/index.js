fb299d9c4d968f5d579ac208aa6ce13da64809d6
matt-plant-auth0 <matthew.plant@auth0.com>
2022-07-28 13:54:05 +0100

    update env vars

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,21 @@ var {
   CLIENT_SECRET,
   SAML_PK,
   SESSION_SECRET,
-  PORT
+  PORT,
+  M2M_CLIENT,
+  M2M_SECRET
 } = require("../env-config");
 
 var ManagementClient = require('auth0').ManagementClient;
 var auth0 = new ManagementClient({
   domain: ISSUER_BASE_URL,
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET
+  clientId: M2M_CLIENT,
+  clientSecret: M2M_SECRET
 });
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  var clientApp = await auth0.getClient({client_id: 'dAdwCCLXEpxFnDcLhOsqKI2SS0B3EoD0'});
+  var clientApp = await auth0.getClient({client_id: CLIENT_ID});
   var samlSettings = clientApp.addons.samlp;
   res.render('index', { title: 'Auth0 SAML Demo', samlSettings: JSON.stringify(samlSettings, null, 4) });
 });
